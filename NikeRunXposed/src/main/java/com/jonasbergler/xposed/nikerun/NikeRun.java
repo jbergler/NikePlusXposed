b41e59e35618ecf4913b76dcfc2302a48028a83b
Krupal Desai <krupaldesai@gmail.com>
2013-12-27 20:02:07 +0530

    Added Sports app button handler and other fixes

--- NikeRunXposed/src/main/java/com/jonasbergler/xposed/nikerun/NikeRun.java
+++ NikeRunXposed/src/main/java/com/jonasbergler/xposed/nikerun/NikeRun.java
@@ -6,6 +6,7 @@ package com.jonasbergler.xposed.nikerun;
 
 
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -33,6 +34,11 @@ public class NikeRun extends Application {
     public static final String INTENT_PAUSE = "com.jonasbergler.xposed.nikerun.pause";
     public static final String INTENT_RESUME = "com.jonasbergler.xposed.nikerun.resume";
     public static final String INTENT_STOP = "com.jonasbergler.xposed.nikerun.stop";
+    public static final String INTENT_CREATE = "com.jonasbergler.xposed.nikerun.create";
+    public static final String INTENT_DESTROY = "com.jonasbergler.xposed.nikerun.destroy";
+    public static final String INTENT_XCMD = "com.jonasbergler.xposed.nikerun.xcmd";
+
+    public static final String MY_PACKAGE = "com.jonasbergler.xposed.nikerun";
 
     public static final String DATA_DURATION = "duration";
     public static final String DATA_DISTANCE = "distance";
@@ -56,6 +62,11 @@ public class NikeRun extends Application {
 
     private Map<String, String> data;
     private Map<String, Map<String, String>> formats;
+
+    private PebbleKit.PebbleDataReceiver sportsDataHandler = null;
+    private int updateCounter;
+
+    public static Object RunCtrlInstance = null;
 
     @Override
     public void onCreate() {
@@ -85,6 +96,7 @@ public class NikeRun extends Application {
 
         // Initialise data
         for (String field : DATA) setData(field, "");
+        updateCounter = 0;
 
         // Setup Debugging
         Timber.plant(new DebugTree());
@@ -160,20 +172,12 @@ public class NikeRun extends Application {
     }
 
     public void setStopped() {
-        //Stop watch Sports App
-        stopWatchApp();
 
         this.state = STATE_STOPPED;
         this.setChanged(true);
     }
 
     public void setRunning() {
-
-        // Customize pebble watch app
-        customizeWatchApp();
-
-        //Start watch Sports App
-        startWatchApp();
 
         this.state = STATE_RUNNING;
         this.setChanged(true);
@@ -209,7 +213,9 @@ public class NikeRun extends Application {
         changed = false;
     }
 
-    // Send a broadcast to launch the Sports App on the connected Pebble
+    /**
+     * Send a broadcast to launch the Sports App on the connected Pebble
+     */
     public void startWatchApp() {
         Timber.d("Starting Watch App");
 
@@ -217,13 +223,17 @@ public class NikeRun extends Application {
         PebbleKit.startAppOnPebble(getApplicationContext(), Constants.SPORTS_UUID);
     }
 
-    // Send a broadcast to close Sports App on the connected Pebble
+    /**
+     * Send a broadcast to close Sports App on the connected Pebble
+     */
     public void stopWatchApp() {
         Timber.d("Stopping Watch App");
         PebbleKit.closeAppOnPebble(getApplicationContext(), Constants.SPORTS_UUID);
     }
 
-    // Set customized data for Sports App
+    /**
+     * Set customized data for Sports App
+     */
     public void customizeWatchApp() {
         Timber.d("Customizing watch Sports App");
         final String customAppName = "Nike+ Running";
@@ -233,23 +243,97 @@ public class NikeRun extends Application {
                 getApplicationContext(), Constants.PebbleAppType.SPORTS, customAppName, customIcon);
     }
 
-    // Push (distance, time, pace) data to be displayed on Pebble's Sports App.
+    /**
+     * Push (distance, time, pace) data to be displayed on Pebble's Sports App.
+     */
     public void updateWatchApp() {
+
+        /**
+         * Start watch Sports App after every 15 seconds
+         * There seems to be a bug in current PebbleSDK2 Beta4
+         * Sports app disappears from menu after some time (if not active)
+         */
+        if(updateCounter > 15) {
+            startWatchApp();
+            updateCounter = 0;
+        }
+        else {
+            updateCounter++;
+        }
 
         String time = getData(NikeRun.DATA_DURATION);
         String distance = getData(NikeRun.DATA_DISTANCE);
         String pace = getData(NikeRun.DATA_PACE);
 
+        PebbleDictionary data = new PebbleDictionary();
+
+        //Differentiate not running state
+        if(this.state != STATE_RUNNING){
+            time = ":" + time + ":";
+        }
+
         Timber.d("Updating Watch App with values: Dur='" + time + "' Dist='" + distance + "' Pace='" + pace + "'");
 
-        PebbleDictionary data = new PebbleDictionary();
         data.addString(Constants.SPORTS_TIME_KEY, time);
         data.addString(Constants.SPORTS_DISTANCE_KEY, distance);
         data.addString(Constants.SPORTS_DATA_KEY, pace);
         data.addUint8(Constants.SPORTS_LABEL_KEY, (byte)Constants.SPORTS_DATA_PACE); // Set pace label
-        data.addUint8(Constants.SPORTS_UNITS_KEY, (byte)Constants.SPORTS_UNITS_METRIC); // Unit metrics
+        data.addUint8(Constants.SPORTS_UNITS_KEY, (byte) Constants.SPORTS_UNITS_METRIC); // Unit metrics
 
         PebbleKit.sendDataToPebble(getApplicationContext(), Constants.SPORTS_UUID, data);
+    }
+
+    /**
+     * Handle Nike+ run created
+     */
+    public void runCreated() {
+
+        Timber.d("New run created");
+
+        // Register handler to receive button events from pebble
+        sportsDataHandler = new PebbleKit.PebbleDataReceiver(Constants.SPORTS_UUID) {
+            @Override
+            public void receiveData(final Context context, final int transactionId, final PebbleDictionary data) {
+                int newState = data.getUnsignedInteger(Constants.SPORTS_STATE_KEY).intValue();
+
+                // Broadcast intent to NikeRunXPosed for changing Nike+ running state
+                Intent intent = new Intent();
+                intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+                intent.setAction(NikeRun.INTENT_XCMD);
+                intent.putExtra("sportsstate", newState);
+
+                //Send intent
+                context.sendBroadcast(intent);
+
+                PebbleKit.sendAckToPebble(context, transactionId);
+
+                Timber.d("Sports App state changed: %d", newState);
+            }
+        };
+        PebbleKit.registerReceivedDataHandler(this, sportsDataHandler);
+
+        // Customize pebble watch app
+        customizeWatchApp();
+
+        // Start watch Sports App
+        startWatchApp();
+    }
+
+    /**
+     * Handle Nike+ run destroyed
+     */
+    public void runDestroyed() {
+
+        Timber.d("Run destroyed");
+
+        //De-register Sports App handler
+        if (sportsDataHandler != null) {
+            unregisterReceiver(sportsDataHandler);
+            sportsDataHandler = null;
+        }
+
+        //Stop watch Sports App
+        stopWatchApp();
     }
 
 }
